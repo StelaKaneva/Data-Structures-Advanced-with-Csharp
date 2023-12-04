aa409c08223c0ff739eb9c76d9e234ed5079b02d
Stela Kaneva <70379843+StelaKaneva@users.noreply.github.com>
2023-12-04 15:29:34 +0200

    Add solutions of exam problems

---
+++ ExamPreparation/Exam/Exam.Categorization/Categorizator.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam.Categorization
+{
+    public class Categorizator : ICategorizator
+    {
+        private Dictionary<string, Category> categories = new Dictionary<string, Category>();
+        public void AddCategory(Category category)
+        {
+            if (this.categories.ContainsKey(category.Id))
+            {
+                throw new ArgumentException();
+            }
+
+            category.Depth = 0;
+
+            categories.Add(category.Id, category);
+        }
+
+        public void AssignParent(string childCategoryId, string parentCategoryId)
+        {
+            if (!categories.ContainsKey(childCategoryId) || !categories.ContainsKey(parentCategoryId))
+            {
+                throw new ArgumentException();
+            }
+
+            var childCategory = this.categories[childCategoryId];
+            var parentCategory = this.categories[parentCategoryId];
+
+            if (parentCategory.Children.Contains(childCategory))
+            {
+                throw new ArgumentException();
+            }
+
+            childCategory.Parent = parentCategory;
+            parentCategory.Children.Add(childCategory);
+
+            var ancestor = parentCategory;
+            while (ancestor.Parent != null)
+            {
+                ancestor = ancestor.Parent;
+            }
+
+            UpdateParentDepth(ancestor);
+        }
+
+        private int UpdateParentDepth(Category node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            var depth = 0;
+
+            foreach (var child in node.Children)
+            {
+                depth = Math.Max(depth, UpdateParentDepth(child));
+            }
+
+            node.Depth = depth + 1;
+
+            return node.Depth;
+        }
+
+        public bool Contains(Category category)
+        {
+            return this.categories.ContainsKey(category.Id);
+        }
+
+        public IEnumerable<Category> GetChildren(string categoryId)
+        {
+            if (!categories.ContainsKey(categoryId))
+            {
+                throw new ArgumentException();
+            }
+
+            var children = new HashSet<Category>();
+
+            GetAllChidren(categoryId, children);
+
+            return children;
+        }
+
+        private void GetAllChidren(string categoryId, HashSet<Category> children)
+        {
+            foreach (var child in categories[categoryId].Children)
+            {
+                children.Add(child);
+                GetAllChidren(child.Id, children);
+            }
+        }
+
+        public IEnumerable<Category> GetHierarchy(string categoryId)
+        {
+            if (!categories.ContainsKey(categoryId))
+            {
+                throw new ArgumentException();
+            }
+
+            var stack = new Stack<Category>();
+
+            GetAllParentsRecursively(categories[categoryId], stack);
+
+            return stack;
+        }
+
+        private void GetAllParentsRecursively(Category category, Stack<Category> stack)
+        {
+            if (category == null)
+            {
+                return;
+            }
+
+            stack.Push(category);
+
+            GetAllParentsRecursively(category.Parent, stack);
+        }
+
+        public IEnumerable<Category> GetTop3CategoriesOrderedByDepthOfChildrenThenByName()
+        {
+            return this.categories.Values
+                            .OrderByDescending(c => c.Depth)
+                            .ThenBy(c => c.Name)
+                            .Take(3);
+        }
+
+        public void RemoveCategory(string categoryId)
+        {
+            if (!categories.ContainsKey(categoryId))
+            {
+                throw new ArgumentException();
+            }
+
+            var category = this.categories[categoryId];
+            categories.Remove(categoryId);
+
+            RemoveChildrenRecursively(category);
+
+            if (category.Parent != null)
+            {
+                category.Parent.Children.Remove(category);
+            }
+        }
+
+        private void RemoveChildrenRecursively(Category category)
+        {
+            foreach (var child in category.Children)
+            {
+                RemoveChildrenRecursively(child);
+                categories.Remove(child.Id);
+            }
+        }
+
+        public int Size()
+            => this.categories.Count;
+    }
+}
---
+++ ExamPreparation/Exam/Exam.TaskManager/TaskManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exam.TaskManager
+{
+
+    public class TaskManager : ITaskManager
+    {
+        //private Dictionary<string, Task> executedTasks;
+        //private Dictionary<string, Task> executableTasks;
+        private LinkedList<Task> taskQueue = new LinkedList<Task>(); // options AddLast & RemoveFirst
+        private HashSet<Task> executedTasks = new HashSet<Task>();
+        private Dictionary<string, Task> allTasks = new Dictionary<string, Task>();
+
+        public void AddTask(Task task)
+        {
+            taskQueue.AddLast(task);
+            allTasks.Add(task.Id, task);
+        }
+
+        public bool Contains(Task task)
+        {
+            return allTasks.ContainsKey(task.Id);
+        }
+
+        public void DeleteTask(string taskId)
+        {
+            if (!allTasks.ContainsKey(taskId))
+            {
+                throw new ArgumentException();
+            }
+
+            var task = allTasks[taskId];
+            allTasks.Remove(taskId);
+
+            if (executedTasks.Contains(task))
+            {
+                executedTasks.Remove(task);
+            }
+            else
+            {
+                taskQueue.Remove(task);
+            }
+        }
+
+        public Task ExecuteTask()
+        {
+            if (taskQueue.Count == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            var task = taskQueue.First.Value;
+
+            taskQueue.RemoveFirst();
+
+            executedTasks.Add(task);
+
+            return task;
+        }
+
+        public IEnumerable<Task> GetAllTasksOrderedByEETThenByName()
+        {
+            return this.allTasks.Values
+                        .OrderByDescending(t => t.EstimatedExecutionTime)
+                        .ThenBy(t => t.Name.Length);
+        }
+
+        public IEnumerable<Task> GetDomainTasks(string domain)
+        {
+            var tasks = taskQueue
+                            .Where(t => t.Domain == domain)
+                            .ToList();
+
+            if (tasks.Count == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            return tasks;
+        }
+
+        public Task GetTask(string taskId)
+        {
+            if (!allTasks.ContainsKey(taskId))
+            {
+                throw new ArgumentException();
+            }
+
+            return allTasks[taskId];
+        }
+
+        public IEnumerable<Task> GetTasksInEETRange(int lowerBound, int upperBound)
+        {
+            return this.taskQueue
+                        .Where(t => t.EstimatedExecutionTime >= lowerBound && t.EstimatedExecutionTime <= upperBound);
+        }
+
+        public void RescheduleTask(string taskId)
+        {
+            if (!allTasks.ContainsKey(taskId))
+            {
+                throw new ArgumentException();
+            }
+
+            var task = allTasks[taskId];
+            executedTasks.Remove(task);
+            taskQueue.AddLast(task);
+        }
+
+        public int Size()
+            => this.taskQueue.Count;
+    }
+}
